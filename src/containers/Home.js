030f0780d68b5c25ad521b50abfa82ba491ccf05
bradserlis <bradserlis@yahoo.com>
2020-01-14 21:44:16 -0800

    Retrieve and render additional character details

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Home = () => {
 
   const [characters, setCharacter] = useState([]);
+  const [mountList, setMountList] = useState([]);
 
-  const retrieveCharacter = (name) => {
-    axios.get(`https://xivapi.com/character/search?name=${name.target.value}&server=cactuar`)
+  // const retrieveCharacter = (name) => {
+  //   axios.get(`https://xivapi.com/character/search?name=${name.target.value}&server=cactuar`)
+  //   .then((res)=>{
+  //     console.log(res.data)
+  //     setCharacter(...characters, res.data.Results[0]);
+  //   })
+  // }
+
+  const retrieveCharacterDetails = (id) => {
+    axios.get(`https://xivapi.com/character/${id.target.value}?data=MIMO`)
     .then((res)=>{
-      console.log(res.data)
-      setCharacter(...characters, res.data.Results[0]);
+      console.log(res.data);
+      setCharacter(...characters, res.data);
+      let mappedMountList = res.data.Mounts.map((mount)=>{
+        return (
+          <li
+          key={mount.Name.toString()}
+          >
+            {mount.Name}
+          </li>
+        ) 
+      })
+      setMountList(...mountList, mappedMountList);
     })
   }
 
@@ -19,24 +38,53 @@ const Home = () => {
           Home Page
         </h1>
         <button
-        onClick={retrieveCharacter}
-        value='Athrun+Lhant'
-        type='button'>
+        onClick={retrieveCharacterDetails}
+        value='5934232'
+        >
         Retrieve Athrun
         </button>
-        {characters.Avatar ? 
+        <button
+        onClick={retrieveCharacterDetails}
+        value='20740039'
+        >
+        Retrieve Shinocoulo
+        </button>
+        {characters.Character ? 
           <div>
             <h3>
-              {characters.Name}
+              {characters.Character.Name}
             </h3>
             <h4>
-              {characters.Server}
+              {characters.Character.Server}
             </h4>
-            <img src={`${characters.Avatar}`} />
-           </div> : null
+            <img 
+            src={`${characters.Character.Portrait}`}
+            alt=''
+            style={{ width:250, height:300 }} 
+            />
+            <h2> Mount List </h2>
+            <ul>
+            { mountList }
+            </ul>
+          </div> : null
         }
       </div>
     )
 }
 
+        // <button
+        // onClick={retrieveCharacter}
+        // value='Athrun+Lhant'
+        // type='button'>
+        // Retrieve Athrun
+        // </button>
+
 export default Home;
+
+            // <ul>
+            //   {characters.Mounts.forEach((mount)=>{
+            //     return(
+            //       <Mount mounts={mount} />
+            //     )
+            //   })}
+            // </ul>
