5421f76f4180cb722ef430330ad6b29bf6551f38
bradserlis <bradserlis@yahoo.com>
2020-01-14 19:30:30 -0800

    Call to FFXIV api, render response just calling one character record currently

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+
+  const [characters, setCharacter] = useState([]);
+
+  const retrieveCharacter = (name) => {
+    axios.get(`https://xivapi.com/character/search?name=${name.target.value}&server=cactuar`)
+    .then((res)=>{
+      console.log(res.data)
+      setCharacter(...characters, res.data.Results[0]);
+    })
+  }
+
   return (
       <div>
         <h1> 
           Home Page
         </h1>
         <button
-        type='primary'>
-        Button?
+        onClick={retrieveCharacter}
+        value='Athrun+Lhant'
+        type='button'>
+        Retrieve Athrun
         </button>
+        {characters.Avatar ? 
+          <div>
+            <h3>
+              {characters.Name}
+            </h3>
+            <h4>
+              {characters.Server}
+            </h4>
+            <img src={`${characters.Avatar}`} />
+           </div> : null
+        }
       </div>
     )
 }
