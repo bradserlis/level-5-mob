8caae5cc92c8eedc60d86d8d173c9c5e90de94bc
bradserlis <bradserlis@yahoo.com>
2020-01-15 20:09:07 -0800

    Route to FFXIV

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home';
+import FFXIV from './containers/FFXIV';
 
 const Router = () => {
   return(
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route path='/ffxiv' component={FFXIV} />
     </Switch>
     )
 }
